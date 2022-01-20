17a2dc050ecc1b2c06e79cc7f725a0b4322665e4
Tim Hurley <tim.hurley@me.com>
2022-01-20 15:38:21 -0700

    add map markers

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,7 @@
 
 <script>
 import mapboxgl from "mapbox-gl";
-import { onMounted, computed } from "vue";
+import { onMounted, computed, ref } from "vue";
 export default {
   props: {
     waypoints: {
@@ -19,24 +19,45 @@ export default {
   setup(props) {
     const accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN;
     const center = props.center;
-
+    let map = {};
     function createMap() {
       try {
         mapboxgl.accessToken = accessToken;
-        const map = new mapboxgl.Map({
+        map = new mapboxgl.Map({
           container: "map",
           style: "mapbox://styles/mapbox/streets-v11",
           center: center,
           zoom: 4,
         });
-
+        addMarkers();
       } catch (err) {
         console.log("map error", err);
       }
     }
 
+    function addMarkers() {
+      locationsToPlot.value.forEach(location => {
+        new mapboxgl.Marker({
+          color: "#54ADDF"
+        })
+        .setLngLat(location)
+        .addTo(map);
+      })
+    }
+
+    const locationsToPlot = computed(() => {
+      return props.waypoints?.reduce((acc, currVal) => {
+        let event = currVal.events.find(event => {
+          return event.location.longitude != false;
+        });
+        acc.push([parseFloat(event.location.longitude), parseFloat(event.location.latitude)]);
+        return acc;
+      }, [])
+    })
+
+
     onMounted(() => createMap());
-    return { };
+    return { locationsToPlot };
   }
 }
 </script>
